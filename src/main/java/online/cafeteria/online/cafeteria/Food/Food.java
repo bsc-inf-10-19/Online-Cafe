d309bc8496365fa82f08329e7bf612c82d01b585
EneyaFrancis <bsc-com-05-19@unima.ac.mw>
2022-01-20 22:36:34 +0800

    final

--- src/main/java/online/cafeteria/online/cafeteria/Food/Food.java
+++ src/main/java/online/cafeteria/online/cafeteria/Food/Food.java
@@ -1,2 +1,51 @@
-package online.cafeteria.online.cafeteria.Food;public class Food {
+package online.cafeteria.online.cafeteria.Food;
+
+public class Food {
+
+    private Long id;
+    private String name;
+    private Integer amount;
+
+    public Food() {
+    }
+
+
+    public Food(Long id, String name, Integer amount) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
--- src/main/java/online/cafeteria/online/cafeteria/Food/FoodController.java
+++ src/main/java/online/cafeteria/online/cafeteria/Food/FoodController.java
@@ -1,2 +1,27 @@
-package online.cafeteria.online.cafeteria.Food;public class FoodController {
+package online.cafeteria.online.cafeteria.Food;
+
+import online.cafeteria.online.cafeteria.student.Student;
+import online.cafeteria.online.cafeteria.student.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/cafe/student/food")
+public class FoodController {
+
+    private final  FoodService foodService;
+
+    @Autowired
+    public FoodController(FoodService foodService) {
+        this.foodService = foodService;
+    }
+
+    @GetMapping
+    public List<Food> getFood() {
+        return  foodService.getfoodlist();
+    }
 }
